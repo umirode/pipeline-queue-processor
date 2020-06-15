f04f304e6c2793ee4c17df38640c870db030e800
maksim <maksim.pavlov@tk-alpha.ru>
2020-06-15 13:32:21 +0300

    add tests for QueueProcessor

---
+++ tests/PipelineQueueProcessor/QueueProcessorTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Umirode\PipelineQueueProcessor\Tests;
+
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Umirode\PipelineQueueProcessor\QueueProcessor;
+use Umirode\PipelineQueueProcessor\QueueStagePayloadPusher;
+
+/**
+ * Class QueueProcessorTest
+ * @package Umirode\PipelineQueueProcessor\Tests
+ */
+final class QueueProcessorTest extends TestCase
+{
+    public function testProcess(): void
+    {
+        /** @var QueueStagePayloadPusher|MockObject $pusher */
+        $pusher = $this->createMock(QueueStagePayloadPusher::class);
+        $pusher->method('push')->willReturn('test_identifier');
+
+        $processor = new QueueProcessor($pusher);
+
+        $result = $processor->process([], ['TestStage']);
+        $this->assertEquals('test_identifier', $result);
+    }
+
+    public function testProcessEmpty(): void
+    {
+        /** @var QueueStagePayloadPusher|MockObject $pusher */
+        $pusher = $this->createMock(QueueStagePayloadPusher::class);
+
+        $processor = new QueueProcessor($pusher);
+
+        $result = $processor->process([]);
+        $this->assertNull($result);
+    }
+}
