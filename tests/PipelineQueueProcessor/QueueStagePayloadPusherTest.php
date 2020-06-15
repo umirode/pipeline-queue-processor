185125a98f834167813c1bd28354ac3e5e8ce0a3
maksim <maksim.pavlov@tk-alpha.ru>
2020-06-15 13:05:34 +0300

    add test for QueueStagePayloadPusher

---
+++ tests/PipelineQueueProcessor/QueueStagePayloadPusherTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Umirode\PipelineQueueProcessor\Tests;
+
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Umirode\PipelineQueueProcessor\QueueInterface;
+use Umirode\PipelineQueueProcessor\QueueStagePayload;
+use Umirode\PipelineQueueProcessor\QueueStagePayloadPusher;
+
+/**
+ * Class QueueStagePayloadPusherTest
+ * @package Umirode\PipelineQueueProcessor\Tests
+ */
+final class QueueStagePayloadPusherTest extends TestCase
+{
+    public function testPushEmptyStagePayload(): void
+    {
+        /** @var QueueInterface|MockObject $queue */
+        $queue = $this->createMock(QueueInterface::class);
+
+        $pusher = new QueueStagePayloadPusher($queue);
+
+        $result = $pusher->push([], new QueueStagePayload());
+        $this->assertNull($result);
+    }
+
+    public function testPushCorrectQueueResponse(): void
+    {
+        /** @var QueueInterface|MockObject $queue */
+        $queue = $this->createMock(QueueInterface::class);
+
+        $pusher = new QueueStagePayloadPusher($queue);
+
+        $stagePayload = new QueueStagePayload(null, 0, [TestStage::class]);
+
+        $queue->method('push')->willReturn(true);
+        $result = $pusher->push([], $stagePayload);
+        $this->assertEquals($stagePayload->getPipelineIdentifier(), $result);
+    }
+
+    public function testPushIncorrectQueueResponse(): void
+    {
+        /** @var QueueInterface|MockObject $queue */
+        $queue = $this->createMock(QueueInterface::class);
+
+        $pusher = new QueueStagePayloadPusher($queue);
+
+        $stagePayload = new QueueStagePayload(null, 0, [TestStage::class]);
+
+        $queue->method('push')->willReturn(false);
+        $result = $pusher->push([], $stagePayload);
+        $this->assertNull($result);
+    }
+}
